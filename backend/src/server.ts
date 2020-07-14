fa010e2b12681b59eb626b9cdf29e7ad3fb7e71f
mariaclarabs <clarabsouza@hotmail.com>
2020-07-14 16:52:21 -0300

    Api: config inicial typescript

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({message: 'Hello GoBarber!'});
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
