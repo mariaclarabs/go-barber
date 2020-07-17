52adcb75b5f791d23da669e8bdf81ce9158390cf
mariaclarabs <clarabsouza@hotmail.com>
2020-07-17 20:29:43 -0300

    Interface e tratamento de horários de agendamentos

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { parseISO, startOfHour, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Array<Appointment> = [];
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(appointment.date, parsedDate),
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
