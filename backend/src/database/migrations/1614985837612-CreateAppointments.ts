64d4c50c7c23b92eaa518fce206109e396a1c868
mariaclarabs <clarabsouza@hotmail.com>
2021-03-07 16:10:12 -0300

    criação de fk usuário no agendamento

--- backend/src/database/migrations/1614985837612-CreateAppointments.ts
+++ backend/src/database/migrations/1614985837612-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1614985837612
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
